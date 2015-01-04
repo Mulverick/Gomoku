70fa408655b1356c3849aa0016e21367aa2fa61f
Maxime Bailly <maxime.bailly@epitech.eu>
2015-01-04 19:17:14 +0100

    Fixes MainMenu

--- Source Files/MainMenu.cpp
+++ Source Files/MainMenu.cpp
@@ -32,14 +32,25 @@ MainMenu::MainMenu(AWidget *parent) :
   _pvp->setFontSize(30);
   _pvp->setText("PVP");
   _pvp->setActive(true);
-  _pvp->setAction([this](){_pvp->setBackgroundTexture("../Assets/button_select.png"); _pve->setBackgroundTexture("../Assets/button_unselect.png");});
+  _pvp->setAction([this] {
+	  _pvp->setBackgroundTexture("../Assets/button_select.png");
+	  _pvp->setActive(true);
+	  _pve->setBackgroundTexture("../Assets/button_unselect.png");
+	  _pve->setActive(false);
+  });
   _pve->setSize(sf::Vector2f(200, 50));
   _pve->setPosition(sf::Vector2f(275, 25));
   _pve->setBackgroundTexture("../Assets/button_unselect.png");
   _pve->setFont("../Assets/fontBambo.ttf");
   _pve->setFontSize(30);
   _pve->setText("PVE");
-  _pve->setAction([this](){_pvp->setBackgroundTexture("../Assets/button_unselect.png"); _pve->setBackgroundTexture("../Assets/button_select.png");});
+  _pve->setActive(false);
+  _pve->setAction([this] {
+	  _pvp->setBackgroundTexture("../Assets/button_unselect.png");
+	  _pvp->setActive(false);
+	  _pve->setBackgroundTexture("../Assets/button_select.png");
+	  _pve->setActive(true);
+  });
   _5breakable->setSize(sf::Vector2f(200, 50));
   _5breakable->setPosition(sf::Vector2f(25, 125));
   _5breakable->setBackgroundTexture("../Assets/button_unselect.png");
@@ -47,7 +58,10 @@ MainMenu::MainMenu(AWidget *parent) :
   _5breakable->setFontSize(30);
   _5breakable->setText("5 Cassables");
   _5breakable->setActive(false);
-  _5breakable->setAction([this](){_5breakable->setBackgroundTexture((_5breakable->isActive() ? "../Assets/button_unselect.png" : "../Assets/button_select.png" )); _5breakable->setActive(!_5breakable->isActive());});
+  _5breakable->setAction([this] {
+	  _5breakable->setBackgroundTexture((_5breakable->isActive() ? "../Assets/button_unselect.png" : "../Assets/button_select.png" ));
+	  _5breakable->setActive(!_5breakable->isActive());
+  });
   _doubleThree->setSize(sf::Vector2f(200, 50));
   _doubleThree->setPosition(sf::Vector2f(275, 125));
   _doubleThree->setBackgroundTexture("../Assets/button_unselect.png");
@@ -55,7 +69,10 @@ MainMenu::MainMenu(AWidget *parent) :
   _doubleThree->setFontSize(30);
   _doubleThree->setText("Double Trois");
   _doubleThree->setActive(false);
-  _doubleThree->setAction([this](){_doubleThree->setBackgroundTexture((_doubleThree->isActive() ? "../Assets/button_unselect.png" : "../Assets/button_select.png" )); _doubleThree->setActive(!_doubleThree->isActive());});
+  _doubleThree->setAction([this] {
+	  _doubleThree->setBackgroundTexture((_doubleThree->isActive() ? "../Assets/button_unselect.png" : "../Assets/button_select.png" ));
+	  _doubleThree->setActive(!_doubleThree->isActive());
+  });
 
   setPosition(sf::Vector2f(149, 249));
   setSize(sf::Vector2f(500, 300));
